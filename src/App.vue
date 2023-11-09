587a5629840a05feb623be7b15affc91f9660b68
Marvin Alejandro Gasca Calderón <marlexgscl@gmail.com>
2023-11-09 14:28:28 -0600

    Commit Inicial

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+    export default {
+        data() {
+            return {
+                titulo: "Plantilla"
+            }
+        }
+    }
+</script>
+<template>
+    <div>Hola Vue 2 {{ titulo }}</div>
+</template>
+<style>
+</style>
